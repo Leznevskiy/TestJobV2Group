a3623d4a49e2c0289ba00d3faf668f055395ef3b
leznevskiy_av_3706 <leznevskiy_av_3706@SDE-58-0>
2023-07-24 16:44:01 +0700

    TestView 1

--- TestJobV2Group/Form1.Designer.cs
+++ TestJobV2Group/Form1.Designer.cs
@@ -29,29 +29,97 @@ namespace TestJobV2Group
         /// </summary>
         private void InitializeComponent()
         {
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.работаСФайламиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.PictureBox = new System.Windows.Forms.PictureBox();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.SearchPointButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripBackButton = new System.Windows.Forms.ToolStripButton();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SearchPointButton,
+            this.toolStripBackButton});
+            this.toolStrip.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip.TabIndex = 1;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.работаСФайламиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // работаСФайламиToolStripMenuItem
+            // 
+            this.работаСФайламиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитьФайлToolStripMenuItem,
+            this.сохранитьФайлToolStripMenuItem});
+            this.работаСФайламиToolStripMenuItem.Name = "работаСФайламиToolStripMenuItem";
+            this.работаСФайламиToolStripMenuItem.Size = new System.Drawing.Size(120, 20);
+            this.работаСФайламиToolStripMenuItem.Text = "Работа с файлами";
+            // 
+            // загрузитьФайлToolStripMenuItem
+            // 
+            this.загрузитьФайлToolStripMenuItem.Name = "загрузитьФайлToolStripMenuItem";
+            this.загрузитьФайлToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.загрузитьФайлToolStripMenuItem.Text = "Загрузить файл";
+            this.загрузитьФайлToolStripMenuItem.Click += new System.EventHandler(this.загрузитьФайлToolStripMenuItem_Click);
+            // 
+            // сохранитьФайлToolStripMenuItem
+            // 
+            this.сохранитьФайлToolStripMenuItem.Name = "сохранитьФайлToolStripMenuItem";
+            this.сохранитьФайлToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьФайлToolStripMenuItem.Text = "Сохранить файл";
+            this.сохранитьФайлToolStripMenuItem.Click += new System.EventHandler(this.сохранитьФайлToolStripMenuItem_Click);
             // 
             // PictureBox
             // 
             this.PictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.PictureBox.Location = new System.Drawing.Point(0, 25);
+            this.PictureBox.Location = new System.Drawing.Point(0, 49);
             this.PictureBox.Name = "PictureBox";
-            this.PictureBox.Size = new System.Drawing.Size(800, 425);
+            this.PictureBox.Size = new System.Drawing.Size(800, 401);
             this.PictureBox.TabIndex = 0;
             this.PictureBox.TabStop = false;
-            this.PictureBox.Click += new System.EventHandler(this.PictureBox_Click);
-            this.PictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox_Paint);
             // 
-            // toolStrip1
+            // SearchPointButton
             // 
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
-            this.toolStrip1.TabIndex = 1;
-            this.toolStrip1.Text = "toolStrip1";
+            this.SearchPointButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.SearchPointButton.Image = global::TestJobV2Group.Properties.Resources._1822947;
+            this.SearchPointButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SearchPointButton.Name = "SearchPointButton";
+            this.SearchPointButton.Size = new System.Drawing.Size(23, 22);
+            this.SearchPointButton.Text = "toolStripButton1";
+            this.SearchPointButton.Click += new System.EventHandler(this.SearchPointButton_Click);
+            // 
+            // toolStripBackButton
+            // 
+            this.toolStripBackButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripBackButton.Image = global::TestJobV2Group.Properties.Resources._17445;
+            this.toolStripBackButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripBackButton.Name = "toolStripBackButton";
+            this.toolStripBackButton.Size = new System.Drawing.Size(23, 22);
+            this.toolStripBackButton.Text = "Удалить последнюю точку";
+            this.toolStripBackButton.Click += new System.EventHandler(this.toolStripBackButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
@@ -59,9 +127,15 @@ namespace TestJobV2Group
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.PictureBox);
-            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -71,7 +145,15 @@ namespace TestJobV2Group
         #endregion
 
         private System.Windows.Forms.PictureBox PictureBox;
-        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton SearchPointButton;
+        private System.Windows.Forms.ToolStripButton toolStripBackButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem работаСФайламиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьФайлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьФайлToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- TestJobV2Group/Form1.cs
+++ TestJobV2Group/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,34 +13,156 @@ namespace TestJobV2Group
 {
     public partial class Form1 : Form
     {
-        private bool isDrawing;
-        private Point startPoint;
-        private Point endPoint;
-        List<Point> points = new List<Point>();
-        List<Point> pointLasts = new List<Point>();
+        /// <summary>
+        /// Список координат для отрисовки фигур
+        /// </summary>
+        private List<Point> points = new List<Point>();
+        /// <summary>
+        /// Проверка выполнение действий. true - рисуем. false - проверяем
+        /// </summary>
+        private bool PointsCheck = true;
+
         public Form1()
         {
             InitializeComponent();
-        }
+            PictureBox.MouseClick += PictureBox_MouseClick;
+            PictureBox.Paint += PictureBox_Paint;
 
-        private void PictureBox_Click(object sender, EventArgs e)
-        {
-            pointLasts.Clear();
-            MouseEventArgs mouseEvent = (MouseEventArgs)e;
-            Point clickedPoint = mouseEvent.Location;
-            points.Add(clickedPoint);
-            PictureBox.Invalidate(); // Перерисовываем PictureBox
-            pointLasts = points;
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
         }
 
         private void PictureBox_Paint(object sender, PaintEventArgs e)
         {
-            if (points.Count >= 2)
+            Graphics g = e.Graphics;
+            // Рисуем точки
+            foreach (Point point in points)
             {
-                using (Pen pen = new Pen(Color.Black))
+                g.FillEllipse(Brushes.Red, point.X - 3, point.Y - 3, 6, 6);
+            }
+            // Соединяем точки линиями
+            if (points.Count > 1)
+            {
+                for (int i = 1; i < points.Count; i++)
                 {
-                    e.Graphics.DrawLines(pen, pointLasts.ToArray());
+                    int Previous = i - 1;
+                    g.DrawLine(Pens.Blue, points[i], points[Previous]);
                 }
+                g.DrawLine(Pens.Blue, points[points.Count -1], points[0]);
+            }
+        }
+
+        private void PictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (PointsCheck) // рисуем
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    points.Add(e.Location); // добавляем новую точку в наш массив
+                    PictureBox.Invalidate(); // Перерисовываем PictureBox для отображения новой точки
+                }
+            }
+            else // проверяем
+            {
+                bool isInside = IsPointInPolygon(e.Location, points);
+                MessageBox.Show("Точка " + (isInside == true ? "внутри" : "снаружи") + " фигуры") ;
+            }
+        }
+
+        private static bool IsPointInPolygon(Point point, List<Point> polygon)
+        {
+            int polygonLength = polygon.Count, i = 0;
+            bool inside = false;
+
+            // За каждую сторону полигона
+            for (int j = polygonLength - 1; i < polygonLength; j = i++)
+            {
+                if (((polygon[i].Y <= point.Y && point.Y < polygon[j].Y) ||
+                    (polygon[j].Y <= point.Y) && (point.Y < polygon[i].Y)) &&
+                    (point.X < (polygon[j].X - polygon[i].X) * (point.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) + polygon[i].X))
+                {
+                    inside = !inside;
+                }
+            }
+            return inside;
+        }
+
+        private void SearchPointButton_Click(object sender, EventArgs e)
+        {
+            PointsCheck = !PointsCheck;
+            if (PointsCheck)
+                MessageBox.Show("Режим рисование фигуры", "v2-Grupp", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Режим проверки точки", "v2-Grupp", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /// <summary>
+        /// убирем последнюю точку
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripBackButton_Click(object sender, EventArgs e)
+        {
+            points.RemoveAt(points.Count -1);
+            PictureBox.Invalidate();
+        }
+
+        private void сохранитьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(points.Count == 0)
+            {
+                MessageBox.Show("Сохранять нечего", "v2-Grupp", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем в файл
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filename))
+                {
+                    for (int i = 0; i < points.Count; i++)
+                    {
+                        writer.WriteLine(points[i].X + "." + points[i].Y);
+                    }
+                }
+                MessageBox.Show("Файл успешно сохранен.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при сохранении файла: " + ex.Message);
+            }
+        }
+
+        private void загрузитьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    return;
+                string filename = openFileDialog1.FileName;
+                // Открываем файл для чтения
+                using (StreamReader sr = new StreamReader(filename))
+                {
+                    string line;
+
+                    // Читаем файл построчно, пока не достигнем конца файла
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] coordinates = line.Split(new char[] { '.' });
+                        Point point = new Point() { X = Convert.ToInt32(coordinates[0]), Y = Convert.ToInt32(coordinates[1]) };
+                        points.Add(point);
+                    }
+                }
+                PictureBox.Invalidate(); // нарисуем
+            }
+            catch (Exception ex)
+            {
+                // Обрабатываем возможные ошибки при чтении файла
+                MessageBox.Show("Ошибка чтения файла:" + ex.Message);
             }
         }
     }

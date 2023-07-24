93a5bbdafcfe0f9c81602e7c8814d62668b4c207
leznevskiy_av_3706 <leznevskiy_av_3706@SDE-58-0>
2023-07-24 14:39:56 +0700

    Добавьте файлы проекта.

---
+++ TestJobV2Group/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace TestJobV2Group
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PictureBox.Location = new System.Drawing.Point(0, 25);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(800, 425);
+            this.PictureBox.TabIndex = 0;
+            this.PictureBox.TabStop = false;
+            this.PictureBox.Click += new System.EventHandler(this.PictureBox_Click);
+            this.PictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox_Paint);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+    }
+}
+
---
+++ TestJobV2Group/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestJobV2Group
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing;
+        private Point startPoint;
+        private Point endPoint;
+        List<Point> points = new List<Point>();
+        List<Point> pointLasts = new List<Point>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            pointLasts.Clear();
+            MouseEventArgs mouseEvent = (MouseEventArgs)e;
+            Point clickedPoint = mouseEvent.Location;
+            points.Add(clickedPoint);
+            PictureBox.Invalidate(); // Перерисовываем PictureBox
+            pointLasts = points;
+        }
+
+        private void PictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (points.Count >= 2)
+            {
+                using (Pen pen = new Pen(Color.Black))
+                {
+                    e.Graphics.DrawLines(pen, pointLasts.ToArray());
+                }
+            }
+        }
+    }
+}
